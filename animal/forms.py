af622396304cb9a028515262c11153fb4730df85
Jadson Silva <jadsonjosesilva@gmail.com>
2024-10-04 20:58:11 -0300

    sinc

--- animal/forms.py
+++ animal/forms.py
@@ -5,4 +5,6 @@ from .models import Animal
 class AnimalForms(forms.ModelForm):
     class Meta:
         model = Animal
-        felds = '__all__'
+        felds = ['name', 'especie', 'raca', 'age', 'porte',
+                 'colour', 'description', 'photo', 'adopted', 'shelter']
+        exclude = ['date_rescue']

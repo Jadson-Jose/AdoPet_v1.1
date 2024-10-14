a32f341cb4cf59c81b317f32370ade28f8d15934
Jadson Silva <jadsonjosesilva@gmail.com>
2024-10-14 14:34:13 -0300

    Update views.py

--- adoption/views.py
+++ adoption/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from adoption.models import Adoption
+from .forms import AdoptionForm
 
-# Create your views here.
+
+def adoption_list(request):
+    adoption = Adoption.objects.all()
+    return render(request, 'adoption_list.html', {'adoption': adoption})
+
+
+def detail_adoption(request, pk):
+    adoption = get_object_or_404(Adoption, pk=pk)
+    return render(request, 'detail_adoption.html', {'adoption': adoption})
